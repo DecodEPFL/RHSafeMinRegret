c8f6f16ca8c3f49928865be5010ef7034960d139
jbrouill <jean-sebastien.brouillon@epfl.ch>
2023-06-19 10:25:35 +0200

    removed useless stuff

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# make a random multiple least squares problem (A, B)
-
-# start loop
-# compute v = biggest singular vector
-# compute E = (I - proj_v) * (AX - B)
-# solve LS of A X - (B+E)
